6eff67c31a5b589087c0478f5780d60322d44651
dcabezas98 <dxabezas@gmail.com>
2018-02-23 13:25:39 +0100

    P1: Loot y SuppliesPackage

--- DeepSpace/src/deepspace/Loot.java
+++ DeepSpace/src/deepspace/Loot.java
@@ -10,5 +10,39 @@ package deepspace;
  * @author dcabezas
  */
 public class Loot {
+
+    int nSupplies;
+    int nWeapons;
+    int nShields;
+    int nHangars;
+    int nMedals;
+    
+    Loot(int sup, int wea, int shi, int han, int med){
+        nSupplies=sup;
+        nWeapons=wea;
+        nShields=shi;
+        nHangars=han;
+        nMedals=med;
+    }
+    
+    public int getNSupplies() {
+        return nSupplies;
+    }
+
+    public int getNWeapons() {
+        return nWeapons;
+    }
+
+    public int getNShields() {
+        return nShields;
+    }
+
+    public int getNHangars() {
+        return nHangars;
+    }
+
+    public int getNMedals() {
+        return nMedals;
+    }
     
 }
--- DeepSpace/src/deepspace/SuppliesPackage.java
+++ DeepSpace/src/deepspace/SuppliesPackage.java
@@ -11,4 +11,30 @@ package deepspace;
  */
 public class SuppliesPackage {
     
+    float ammoPower;
+    float fuelUnits;
+    float shieldPower;
+    
+    SuppliesPackage(float amm, float fue, float shi){
+        ammoPower=amm;
+        fuelUnits=fue;
+        shieldPower=shi;
+    }
+
+    SuppliesPackage(SuppliesPackage original){
+        ammoPower=original.ammoPower;
+        fuelUnits=original.fuelUnits;
+        shieldPower=original.shielPower;
+    }
+    public float getAmmoPower() {
+        return ammoPower;
+    }
+
+    public float getFuelUnits() {
+        return fuelUnits;
+    }
+
+    public float getShielPower() {
+        return shielPower;
+    }
 }
